417238e0c0c385e4e7db7bcf4b35770c6b946ca9
Kate Ulasik <>
2021-04-13 14:39:27 +0300

    Added music sequence

--- 210305_Kate_Classes_Begin/main.swift
+++ 210305_Kate_Classes_Begin/main.swift
@@ -9,53 +9,63 @@ import Foundation
 
 
 var linkedList = LinkedList<Int>()
+linkedList.append(68)
+linkedList.append(68)
+linkedList.append(68)
+linkedList.append(69)
 
-print("Program begun...")
+let description = linkedList.description
+print("OUR Linked List Now \(description)")
 
-var quit: Bool = false
+MusicPlayerManager.sharedInstance.playNotes(notes: linkedList)
+sleep(3)
 
-while quit == false {
-    print("Type command ('push', 'append', 'print', 'play' or 'exit'): ")
-    let command = readLine()
-
-    switch command?.lowercased() {
-    case "exit":
-        quit = true
-    case "push":
-        pushCommand()
-    case "append":
-        appendCommand()
-    case "print":
-        printCommand()
-    case "play":
-        playCommand()
-    default:
-        print("Unknown command...")
-    }
-}
-
-print("Program ended...")
-
-func pushCommand() {
-    let stringOptionalNumValue = readLine()
-    
-    if let stringNumValue = stringOptionalNumValue, let num = Int(stringNumValue) {
-        linkedList.push(num)
-    } else {
-        print("Num push error casting...")
-    }
-}
-
-func appendCommand() {
-    
-}
-
-func printCommand() {
-    print(linkedList.description)
-}
-
-func playCommand() {
-    MusicPlayerManager.sharedInstance.playNotes(notes: linkedList)
-}
-
-
+//print("Program begun...")
+//
+//var quit: Bool = false
+//
+//while quit == false {
+//    print("Type command ('push', 'append', 'print', 'play' or 'exit'): ")
+//    let command = readLine()
+//
+//    switch command?.lowercased() {
+//    case "exit":
+//        quit = true
+//    case "push":
+//        pushCommand()
+//    case "append":
+//        appendCommand()
+//    case "print":
+//        printCommand()
+//    case "play":
+//        playCommand()
+//    default:
+//        print("Unknown command...")
+//    }
+//}
+//
+//print("Program ended...")
+//
+//func pushCommand() {
+//    let stringOptionalNumValue = readLine()
+//
+//    if let stringNumValue = stringOptionalNumValue, let num = Int(stringNumValue) {
+//        linkedList.push(num)
+//    } else {
+//        print("Num push error casting...")
+//    }
+//}
+//
+//func appendCommand() {
+//
+//}
+//
+//func printCommand() {
+//    print(linkedList.description)
+//}
+//
+//func playCommand() {
+//    MusicPlayerManager.sharedInstance.playNotes(notes: linkedList)
+//}
+//
+//
